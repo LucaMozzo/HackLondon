fb5521d8a8490e5e8ad1b06dab1fe9e778dcc038
Michal Szewczak <michaelszewczak@gmail.com>
2016-02-27 15:42:46 +0000

    add serving files from root directory

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app.debug = True
 def hello():
     return send_from_directory("","index.html")
 
+@app.route("/<filename>")
+def serve_file(filename):
+    return send_from_directory("", filename)
+
+@app.route("/somethingForTest")
+def test():
+    return "Testing"
+
 if __name__ == "__main__":
     app.run()
