feacb61dece1743f5034dbcc63376139fdef116e
luca <lucamozzo21173@gmail.com>
2016-02-27 18:55:51 +0000

    added names to the fields

--- calendar.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by luca on 27/02/16.
- */
-$(document).ready(function(){
-    //dropdown changed
-    $('#users').children().click(function(){
-        $('#flatmate').value(this.value());
-    });
-});
